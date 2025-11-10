dcde121d674b3f5f73c42254ad36de36885c8856
grafgustav <grafgustav@rambler.ru>
2025-11-10 15:28:17 +0400

    added missing ui files

---
+++ ui/ui_canvas_layer.gd
@@ -0,0 +1,7 @@
+extends CanvasLayer
+
+@onready var mouse_label: Label = %CoordinatesLabel
+
+func _process(_delta):
+	var global_pos = get_viewport().get_mouse_position()
+	mouse_label.text = "(%.1f, %.1f)" % [global_pos.x, global_pos.y]
